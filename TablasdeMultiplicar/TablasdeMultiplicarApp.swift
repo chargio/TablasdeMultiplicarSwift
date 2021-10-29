373a02cdd47ed192b1d6c4e1c020fef7ac148419
Sergio Ocón-Cárdenas <sergio.ocon@redhat.com>
2021-10-29 20:04:49 +0200

    Initial Commit

---
+++ TablasdeMultiplicar/TablasdeMultiplicarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablasdeMultiplicarApp.swift
+//  TablasdeMultiplicar
+//
+//  Created by Sergio Ocón Cárdenas on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TablasdeMultiplicarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
